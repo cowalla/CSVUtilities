fa030df603b871b3ea5c8ad5b258d5f09a4b91aa
Connor Wallace <wallaconno@gmail.com>
2019-02-15 16:16:10 -0700

    make distributable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+__version__ = '0.0.0'
+
+
+setup(
+    name='csv_utilities',
+    author='Connor Wallace',
+    version=__version__,
+    packages=find_packages(),
+)
